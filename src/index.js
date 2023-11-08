dd8b356a778e09892c82befa6bc74972d9a7eafd
root <root@Ubuntu.laelijah.dev>
2023-11-08 11:56:10 -0600

    test

--- src/index.js
+++ src/index.js
@@ -180,53 +180,55 @@ wsServer.on("connection", async (socket, req) => {
 
     socket.onmessage = async (msg) => {
 
-        const decodedMessage = JSON.parse(msg.toString())
-        let { sender, type } = decodedMessage
-        console.log("message sender: ", sender, 185)
+        if (typeof msg === "string") {
+            const decodedMessage = JSON.parse(msg.toString())
+            let { sender, type } = decodedMessage
+            console.log("message sender: ", sender, 185)
 
-        switch (type) {
-            case "handshake":
-                socket.isClosed = false
+            switch (type) {
+                case "handshake":
+                    socket.isClosed = false
 
-                let { sender, groupId } = decodedMessage
-                if (groupId !== "none") {
-                    const key = `${sender}&${ip}`
-                    clients.set(key, socket)
-                    console.log(sender, "connected", 195)
+                    let { sender, groupId } = decodedMessage
+                    if (groupId !== "none") {
+                        const key = `${sender}&${ip}`
+                        clients.set(key, socket)
+                        console.log(sender, "connected", 195)
 
-                    if (groups.get(groupId)) groups.set(groupId, new Set([...Array.from(groups.get(groupId)), key]))
-                    else groups.set(groupId, new Set([key]))
-
-
-                    // Implementing this to get benifits from horizontal scaling
-                    // Needs sticky sessions to get benifits
+                        if (groups.get(groupId)) groups.set(groupId, new Set([...Array.from(groups.get(groupId)), key]))
+                        else groups.set(groupId, new Set([key]))
 
 
-                    // tells client whos in the group right now
-                    groups.get(groupId).forEach((client) => {
-
-                        // add a notification function here for if the client ready state is closed so make it an else statement
-                        if (clients.get(client)?.readyState === ws.OPEN) {
-                            clients.get(client).send(JSON.stringify({
-                                type: "members",
-                                members: groups.get(groupId)
-                            }))
-                        }
-                    })
-
-                }
+                        // Implementing this to get benifits from horizontal scaling
+                        // Needs sticky sessions to get benifits
 
 
+                        // tells client whos in the group right now
+                        groups.get(groupId).forEach((client) => {
 
-                break;
+                            // add a notification function here for if the client ready state is closed so make it an else statement
+                            if (clients.get(client)?.readyState === ws.OPEN) {
+                                clients.get(client).send(JSON.stringify({
+                                    type: "members",
+                                    members: groups.get(groupId)
+                                }))
+                            }
+                        })
 
-            case "message":
+                    }
 
-                await queueEvent(decodedMessage, "messaging-service")
 
-                break;
-            // if the reciver is not on the server, send the event to the notification bus too 
-            // if (!clients[decodedMessage.payload.receiver]) await queueEvent(decodedMessage.payload, "notification-service")
+
+                    break;
+
+                case "message":
+
+                    await queueEvent(decodedMessage, "messaging-service")
+
+                    break;
+                // if the reciver is not on the server, send the event to the notification bus too 
+                // if (!clients[decodedMessage.payload.receiver]) await queueEvent(decodedMessage.payload, "notification-service")
+            }
         }
     }
 
