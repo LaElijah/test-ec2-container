643964d63ebc0d6ac5c3f026f61e7212be03f74f
root <root@Ubuntu.laelijah.dev>
2023-11-17 11:56:55 -0600

    test

--- src/index.js
+++ src/index.js
@@ -4,23 +4,57 @@ import dotenv from 'dotenv';
 import wss from "./_utils/sockets/wss.js"
 import app from "./appConfig.js"
 import consumerHandler from "./_utils/kafka/consumerHandler.js"
+import os from "os"
+import cluster from "cluster";
+
+const cores = os.cpus().length
 
 
-dotenv.config();
-const port = process.env.PORT || 8080
 
-consumer.run({ eachMessage: consumerHandler })
 
-const httpServer = app.listen(port, async () => {
-    await dbConnection()
-    console.log(`Server is running at ${port}`)
-})
 
-// TODO: Update this to run on routes for seperation of notifications and messages sockets
 
-httpServer.on("upgrade", (req, socket, head) => {
-    wss.handleUpgrade(req, socket, head, async (socket) => {
-        wss.emit("connection", socket, req)
+
+if (cluster.isPrimary) {
+    const numCPUs = os.cpus().length
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.error(`Worker ${worker.process.pid} died with code: ${code}, signal: ${signal}`);
+        cluster.fork();
+    });
+
+    cluster.on('error', (error) => {
+        console.error(`Worker error: ${error.message}`);
+    });
+
+}
+
+else {
+
+
+
+
+    // Core seperate
+    dotenv.config();
+    const port = process.env.PORT || 8080
+
+
+    consumer.run({ eachMessage: consumerHandler })
+
+    const httpServer = app.listen(port, async () => {
+        await dbConnection()
+        console.log(`Server is running at ${port}`)
     })
-})
 
+    // TODO: Update this to run on routes for seperation of notifications and messages sockets
+
+    httpServer.on("upgrade", (req, socket, head) => {
+        wss.handleUpgrade(req, socket, head, async (socket) => {
+            wss.emit("connection", socket, req)
+        })
+    })
+
+}
