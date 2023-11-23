646b26b34eae2250e5b31950e4a09a6420db92be
root <root@Ubuntu.laelijah.dev>
2023-11-23 12:21:30 -0600

    test

--- src/_utils/kafka/consumerHandler.js
+++ src/_utils/kafka/consumerHandler.js
@@ -34,9 +34,9 @@ export default async function consumerHandler({message: event, partition}) { // 
                 })
 
                 // Update user history here
-                console.log(event.UUID, "TEST", dbGroup.messages.find(message => message.UUID === event.UUID))
+                console.log(event.UUID, "TEST", !(dbGroup.messages.find(message => message.UUID === event.UUID)))
                 
-                if (!dbGroup.messages.find(message => message.UUID === event.UUID)) {
+                if (!(dbGroup.messages.find(message => message.UUID === event.UUID))) {
                     console.log("SAVING MESSAGE")
                     dbGroup.messages.push({ ...event })
                     await dbGroup.save()
